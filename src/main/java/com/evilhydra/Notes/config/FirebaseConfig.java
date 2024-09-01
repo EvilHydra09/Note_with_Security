95d0b77d5659462833fe1d70981cff24c67fca41
abhidnyasonawane <abhidnya09@gmail.com>
2024-09-01 13:10:33 +0530

    Added the enviroment variable

--- src/main/java/com/evilhydra/Notes/config/FirebaseConfig.java
+++ src/main/java/com/evilhydra/Notes/config/FirebaseConfig.java
@@ -5,33 +5,41 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
 import javax.annotation.PostConstruct;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.Base64;
 
 @Configuration
 public class FirebaseConfig {
 
     private static final Logger logger = LoggerFactory.getLogger(FirebaseConfig.class);
 
-    @Value("${firebase.config.path}")
-    private String configPath;
-
     @PostConstruct
     public void initialize() throws IOException {
-        ClassPathResource serviceAccount = new ClassPathResource(configPath);
+        // Get the base64-encoded Firebase credentials from the environment variable
+        String firebaseCredentialsBase64 = System.getenv("FIREBASE_CREDENTIALS");
 
+        if (firebaseCredentialsBase64 == null || firebaseCredentialsBase64.isEmpty()) {
+            logger.error("Firebase credentials are not set in environment variables.");
+            throw new IllegalStateException("Firebase credentials are not set in environment variables.");
+        }
 
+        // Decode the base64-encoded credentials
+        byte[] decodedCredentials = Base64.getDecoder().decode(firebaseCredentialsBase64);
+
+        // Initialize Firebase with the decoded credentials
         FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount.getInputStream()))
+                .setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(decodedCredentials)))
                 .build();
 
         if (FirebaseApp.getApps().isEmpty()) {
             FirebaseApp.initializeApp(options);
-            logger.info("App name {}:",FirebaseApp.getInstance().getName());
+            logger.info("Firebase app initialized with name: {}", FirebaseApp.getInstance().getName());
+        } else {
+            logger.info("Firebase app already initialized with name: {}", FirebaseApp.getInstance().getName());
         }
     }
 }
